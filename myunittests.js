0605bda06398c0f2d0884e9ed387dd593931cc9d
criptik <tom.deneau@gmail.com>
2021-09-05 16:01:17 -0500

    myunittests.js merge

--- myunittests.js
+++ myunittests.js
@@ -11,6 +11,31 @@ T, ----1D1N--, 2C, ask for 5-card major; could be weak
 T, 1D----1N--, 2C, ask for 5-card major; could be weak
 T, 1D----1N--, 2D, Jacoby transfer; 5+!Hs
 T, 1H--3S--4C--, 4D,  
+T, ,         1N, 15-17 Balanced could have 5!H or 5!S
+T, --,        +, +
+T, ----,      +, 15-17 Balanced no 5M
+T, ------,    +, +
+T, --1N--, 2C, ask for 5-card major; could be weak
+T, 1N--,    +, +
+--------  Drury 4th suit Exceptions (3rd 4th seat opening only) ---------
+T, 1H--2C--2D--,       2S, 4th suit forcing; might not have !S
+T, --1H--2C--2D--,     2S, 4th suit forcing; might not have !S
+T, ----1H--2C--2D--,   2S, natural
+T, ------1H--2C--2D--, 2S, natural
+--------  Splinter 4th suit Exceptions ---------
+T, 1H--3S--4C--,       4D, 
+----------------------------------------------
+T, 1C--1D--1H--,       2S, 4th suit forcing; might not have !S
+T, 1C--1D--1H--,       1S, natural; at least 4 !S
+T, 1H--,               2N, good !H raise; no singletons or void
+T, 1H--2N--,           3C, shortness in !c
+T, 1N 2C,              2D, natural 5+!D; not forcing
+T, ,        2N, 20-21 HCP Balanced; could have 5!H or 5!S
+T,2C--**--, 2N, 22-24 HCP Balanced; could have 5!H or 5!S
+T,1N 2D,    2N, asks partner to bid 3!C; then I will show my suit
+T,1N 2H,    2N, asks partner to bid 3!C; then I will show my suit
+T,1N 2S,    2N, asks partner to bid 3!C; then I will show my suit
+T, ,                   1H, 
     `;
     
     
